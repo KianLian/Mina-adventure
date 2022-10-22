9cc7343e7688744cd837b5e61adbbcf976341923
Kian Liang <72865440+KianLian@users.noreply.github.com>
2022-10-22 18:49:59 +0100

    Collectible logic ig

--- MinaGame/Assets/Script/Objects/Collectible/Collectible.cs
+++ MinaGame/Assets/Script/Objects/Collectible/Collectible.cs
@@ -2,27 +2,38 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public enum CollectibleType
 {
-    Gt_Money,
-    Gt_Powerup,
-
+    GtMoney,
+    GtPowerUp,
+    GtStamina,
 }
 
 public abstract class Collectible : MonoBehaviour
 {
 
-    protected bool bCanBePicked = false;
+    // protected bool bCanBePicked = false;
+
     [SerializeField]
     protected CollectibleType type;
 
-    private void OnCollisionEnter2D(Collision2D otherCollider)
+    protected ParticleSystem particleSystem = null;
+    
+    protected virtual void Awake()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    protected virtual void OnCollisionEnter2D(Collision2D otherCollider)
     {
         if (otherCollider.gameObject.CompareTag("Player"))
         {
-
+           // GameManager.instance.PickUpPlayer();
         }
-            //PickUp();
+          
     }
+
+
 }
--- MinaGame/Assets/Script/Objects/Collectible/GiftCollectible.cs
+++ MinaGame/Assets/Script/Objects/Collectible/GiftCollectible.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GiftCollectible : Collectible
 {
+   
+
     // Start is called before the first frame update
     void Start()
     {
